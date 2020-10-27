534a9d8dad3fee1983cb7a9d0587854ef95a79e0
Necrid <necridx@gmail.com>
2020-10-27 16:38:33 -0400

    mobile navbar

--- client/src/components/SiteFooter/SiteFooter.jsx
+++ client/src/components/SiteFooter/SiteFooter.jsx
@@ -13,16 +13,12 @@ const SiteFooter = () => {
         <Col xs={12} lg className="my-auto">
           <p className="text-light">
             <small>
-              All FINAL FANTASY&reg; XI content and images &copy; 2002-2020{' '}
-              <a href="https://www.square-enix.com/" className="text-white">
-                SQUARE ENIX
-              </a>{' '}
-              CO., LTD. All Rights Reserved. <br /> FINAL FANTASY&reg; is a
-              registered trademark of{' '}
-              <a href="https://www.square-enix.com/" className="text-white">
-                SQUARE ENIX
-              </a>{' '}
-              CO., LTD. All Rights Reserved.
+            All FINAL FANTASY&reg; XI content and images &copy; 2002-
+              {new Date().getFullYear()}
+              SQUARE ENIX CO., LTD. All Rights Reserved.
+              <br />
+              FINAL FANTASY&reg; is a registered trademark of SQUARE ENIX CO.,
+              LTD. All Rights Reserved.
             </small>
           </p>
         </Col>
