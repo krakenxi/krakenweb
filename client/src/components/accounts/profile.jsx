4e2148d296c735ff08e440d62b77e4bf072838e2
Necrid <necridx@gmail.com>
2021-10-27 16:38:53 -0400

    Update profile.jsx

--- client/src/components/accounts/profile.jsx
+++ client/src/components/accounts/profile.jsx
@@ -13,6 +13,9 @@ export default ({ user, logout, reload }) => {
   const [password, setPassword] = React.useState('');
   const lastLogin = new Date(user.timelastmodify);
 
+  const [player, setPlayer] = React.useState(null);
+  const [equip, setEquip] = React.useState(null);
+
   const updateField = (field, value) => {
     apiUtil.put(
       {
