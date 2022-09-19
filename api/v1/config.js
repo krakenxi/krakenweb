546c78c0e467d938434530bd3e729d7be553b9f1
Necrid <necridx@gmail.com>
2022-09-19 18:38:41 -0400

    Update config.js

--- api/v1/config.js
+++ api/v1/config.js
@@ -69,13 +69,11 @@ module.exports = {
         { item: 'Recast', windower: 'Renamer', allowed: true },
         { item: 'Timestamp', allowed: true },
         {
-          item:
-            'Shorthand (only for actions on players -- NOT allowed for claiming)',
+          item: 'Shorthand (only for actions on players -- NOT allowed for claiming)',
           allowed: true,
         },
         {
-          item:
-            'Renamer (only using the lists we provide -- NOT for custom naming of mobs)',
+          item: 'Renamer (only using the lists we provide -- NOT for custom naming of mobs)',
           allowed: true,
         },
       ],
@@ -103,30 +101,25 @@ module.exports = {
   links: [
     {
       url: 'https://discord.gg/DSa2Q2',
-      image:
-        'https://vignette.wikia.nocookie.net/ffxi/images/0/08/Flag.jpg/revision/latest?cb=20060211183144',
+      image: 'https://vignette.wikia.nocookie.net/ffxi/images/0/08/Flag.jpg/revision/latest?cb=20060211183144',
       header: 'Kraken Discord',
-      description:
-        'Join us on Discord for technical support, up-to-date annoucements, community events, memes, live chat with staff, and more.',
+      description: 'Join us on Discord for technical support, up-to-date annoucements, community events, memes, live chat with staff, and more.',
     },
     {
       url: 'https://github.com//community/issues',
       image: 'https://static.ffxiah.com/images/icon/17001.png',
       header: 'Bug Reports',
-      description:
-        'Kraken has bugs too. :( See what issues we have and chime in if you found a bug!',
+      description: 'Kraken has bugs too. :( See what issues we have and chime in if you found a bug!',
     },
     {
       url: 'https://github.com//issues',
       image: 'https://static.ffxiah.com/images/icon/17005.png',
       header: 'Website Bugs',
-      description:
-        'In addition to technical bugs, Kraken refines rules, information, and connection details occassionally. Chime in if you found an error!',
+      description: 'In addition to technical bugs, Kraken refines rules, information, and connection details occassionally. Chime in if you found an error!',
     },
     {
       url: 'https://ffxiclopedia.wikia.com/wiki/Main_Page',
-      image:
-        'https://vignette.wikia.nocookie.net/ffxi/images/b/b7/Favicon_Poroggo.png/revision/latest/scale-to-width-down/60?cb=20170308121952',
+      image: 'https://vignette.wikia.nocookie.net/ffxi/images/b/b7/Favicon_Poroggo.png/revision/latest/scale-to-width-down/60?cb=20170308121952',
       header: 'FFXIclopedia',
       description:
         'The defacto go-to resource of this era. Keep in mind that this site has been updated post-era and you may need to browse history of the page to get more accurate data.',
@@ -135,22 +128,19 @@ module.exports = {
       url: 'https://www.bg-wiki.com/',
       image: 'https://static.ffxiah.com/images/icon/15076.png',
       header: 'BG Wikipedia',
-      description:
-        "Another resource, however more updated that the era wiki. Can be a good place to get information that isn't provided by other sources.",
+      description: "Another resource, however more updated that the era wiki. Can be a good place to get information that isn't provided by other sources.",
     },
     {
       url: 'http://ffxi-atlas.mageringnetwork.com/',
       image: 'https://static.ffxiah.com/images/icon/191.png',
       header: 'FFXI Atlas (Mirror)',
-      description:
-        'All the maps without the cost of buying your own maps in game. However, good luck finding where you are in relation to the map!',
+      description: 'All the maps without the cost of buying your own maps in game. However, good luck finding where you are in relation to the map!',
     },
     {
       url: 'http://www.pyogenes.com/ffxi/timer/v2.html',
       image: 'https://static.ffxiah.com/images/icon/3706.png',
       header: 'Pyogenes Timer',
-      description:
-        "Pyogenes developed a Vana'Diel clock for retail. Kraken runs on the same timer.",
+      description: "Pyogenes developed a Vana'Diel clock for retail. Kraken runs on the same timer.",
     },
     {
       url: 'http://www.ffxidb.com/',
